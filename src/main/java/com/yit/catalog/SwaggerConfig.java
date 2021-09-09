fe55a62364f154ae6ba774ac2d7b22c63b1c2553
Kriangsak Pornpantiva <kriangsak.po@yipintsoi.com>
2021-09-10 03:37:26 +0700

    update nexus url May

--- src/main/java/com/yit/catalog/SwaggerConfig.java
+++ src/main/java/com/yit/catalog/SwaggerConfig.java
@@ -1,12 +1,8 @@
 package com.yit.catalog;
 
-import java.util.ArrayList;
-import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-
 import com.google.common.base.Predicate;
 import com.google.common.base.Predicates;
 import springfox.documentation.builders.ApiInfoBuilder;
@@ -15,29 +11,11 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger.web.InMemorySwaggerResourcesProvider;
-import springfox.documentation.swagger.web.SwaggerResource;
-import springfox.documentation.swagger.web.SwaggerResourcesProvider;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
 
-	@Primary
-    @Bean
-    public SwaggerResourcesProvider swaggerResourcesProvider(InMemorySwaggerResourcesProvider defaultResourcesProvider) {
-        return () -> {
-            SwaggerResource wsResource = new SwaggerResource();
-            wsResource.setName("Documentation");
-            wsResource.setSwaggerVersion("2.0");
-            wsResource.setLocation("/swagger.yaml");
-
-            List<SwaggerResource> resources = new ArrayList<>(defaultResourcesProvider.get());
-            resources.add(wsResource);
-            return resources;
-        };
-    }
-	
 	@Bean
 	public Docket produceApi() {
 		return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo()).select()
